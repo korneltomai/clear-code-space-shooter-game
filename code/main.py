3e4567b6abfa6d6227e6a1fd3fac0a466550fd64
T3tr1s <tomai.kornelmunka@gmail.com>
2025-05-14 00:15:04 +0200

    Rectangles

--- code/main.py
+++ code/main.py
@@ -16,9 +16,19 @@ x = 100
 y = 150
 
 # importing an image
-player_surface = pygame.image.load(join("images", "player.png")).convert_alpha()
-star_surface = pygame.image.load(join("images", "star.png")).convert_alpha()
+player_surf = pygame.image.load(join("images", "player.png")).convert_alpha()
+player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = 1
+
+star_surf = pygame.image.load(join("images", "star.png")).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
+
+meteor_surf = pygame.image.load(join("images", "meteor.png")).convert_alpha()
+meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+laser_surf = pygame.image.load(join("images", "laser.png")).convert_alpha()
+laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
+
 
 running = True
 while running:
@@ -31,10 +41,16 @@ while running:
     display_surface.fill("black")
     
     for pos in star_positions:
-        display_surface.blit(star_surface, pos)
+        display_surface.blit(star_surf, pos)
 
-    x += 0.1
-    display_surface.blit(player_surface, (x, y))
+    display_surface.blit(meteor_surf, meteor_rect)
+    display_surface.blit(laser_surf, laser_rect)
+
+    # player movement
+    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
+        player_direction *= -1
+    player_rect.right += 0.5 * player_direction
+    display_surface.blit(player_surf, player_rect)
 
     pygame.display.update()
 
